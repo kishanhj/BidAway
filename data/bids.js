2ead01c85ec6199c067bc402ed19b92393f80b8a
kishan <Kash.hj94@gmail.com>
2019-11-30 18:07:37 -0500

    countdown

--- data/bids.js
+++ data/bids.js
@@ -10,19 +10,32 @@ var ObjectId = require('mongodb').ObjectId;
 const addBid = async function addBid(bidInput){
 
     if(!bidInput.user_id || ! typeof bidInput.user_id === "string") throw "Invalid User Id";
-    if(!bidInput.starting_time || ! typeof bidInput.starting_time === "object") throw "Invalid Starting time";
     if(!bidInput.starting_price || ! typeof bidInput.starting_price === "number") throw "Invalid Strating Price";
     if(!bidInput.category || ! Array.isArray(bidInput.category) || bidInput.category.length === 0) throw "Error in category";
     if(!bidInput.item) throw "Error in Item"
     if(! typeof bidInput.item in ['string','object']) throw "You must provide a string or objectId for item";
-    if(!bidInput.ending_time || ! typeof bidInput.ending_time === "object") throw "Error ending time";
+    if(!bidInput.time_period || ! typeof bidInput.time_period === "object") throw "Error ending time";
 
+    const date = new Date();
+    const time = parseInt(bidInput.time_period);
+    date.setHours(date.getHours() + time);
+
+    const newBid = {
+        "user_id" : bidInput.user_id,
+        "starting_price" : bidInput.starting_price,
+        "category" : bidInput.category,
+        "starting_time" : new Date(),
+        "ending_time":date,
+        "item_id":bidInput.item
+
+    }
 
     const bidsCollection = await bidsCollectionObj();
-    const insertInfo = await bidsCollection.insertOne(bidInput);
+    const insertInfo = await bidsCollection.insertOne(newBid);
     if (insertInfo.insertedCount === 0) throw "Could not create Bid";
   
     const newId = insertInfo.insertedId;
+
     const addedBid = await getBidByID(newId);
     return addedBid;
 }
@@ -123,19 +136,16 @@ const getAllActiveBids = async function getAllActiveBids() {
 
     const allbidsObj = [];
     const itemsDataApi = require("./items");
+    const userDataApi = require("./user");
 
     for(let bid of allbids){
 
-        let itemForBid = {};
-        try{
-            itemForBid = await itemsDataApi.getItemById(bid.item);
-        } catch(err) {
-            throw "No item"
-        }
+        const itemForBid = await itemsDataApi.getItemById(bid.item);
+        const user = await userDataApi.getuser(bid.user_id);
 
         const bidObj = {
             "_id" : bid._id,
-            "user_id" : bid.user_id,
+            "username" : user.username,
             "starting_price": bid.starting_price,
             "starting_time": bid.starting_time,
             "ending_time": bid.ending_time,
@@ -157,27 +167,23 @@ const getBidByCategory = async function getBidByCategory(categoryInput){
 
     if(!categoryInput || typeof categoryInput !== 'string') throw "You must provide a proper category";
     const bidsCollection = await bidsCollectionObj();
-    let activeBids = await bidsCollection.find({category:categoryInput}).toArray();
+    let allbids = await bidsCollection.find({category:categoryInput}).toArray();
 
     const allbidsObj = [];
     const itemsDataApi = require("./items");
+    const userDataApi = require("./user");
 
-    for(let bid of activeBids){
+    for(let bid of allbids){
 
-        let itemForBid = {};
-        try{
-            itemForBid = await itemsDataApi.getItemById(bid.item);
-        } catch(err) {
-            throw "No item"
-        }
+        const itemForBid = await itemsDataApi.getItemById(bid.item);
+        const user = await userDataApi.getuser(bid.user_id);
 
         const bidObj = {
             "_id" : bid._id,
-            "user_id" : bid.user_id,
+            "username" : user.username,
             "starting_price": bid.starting_price,
             "starting_time": bid.starting_time,
-            "time_period": bid.time_period,
-            "is_active":bid.is_active,
+            "ending_time": bid.ending_time,
             "category":bid.category,
             "item":itemForBid
         }
