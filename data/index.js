2f5b4208621a9727310b84692a169b526b9237e3
Abin007 <abinmartin.jones@gmail.com>
2019-12-11 18:01:36 -0500

    remove testing code

--- data/index.js
+++ data/index.js
@@ -2,26 +2,26 @@ const userdata=require("./user")
 const commments=require("./comments")
 const items= require("./items")
 
-async function main(){
-    try{
-        const item={name: 'Google Home',
-        description: 'I know what you did last summer',
-        category: 'Electronics',
-        startPrice: 69.69,
-        startTime: new Date('2019-11-19 11:11')
-    }
-        const comment= await items.addItem(item.name,item.category,item.category,item.startPrice,item.startTime,"5dcc84bdfb6508dd6dc49e77")
-        console.log(comment)
+// async function main(){
+//     try{
+//         const item={name: 'Google Home',
+//         description: 'I know what you did last summer',
+//         category: 'Electronics',
+//         startPrice: 69.69,
+//         startTime: new Date('2019-11-19 11:11')
+//     }
+//         const comment= await items.addItem(item.name,item.category,item.category,item.startPrice,item.startTime,"5dcc84bdfb6508dd6dc49e77")
+//         console.log(comment)
         
 
-    }
-    catch(e){
-        console.log(e)
-    }
+//     }
+//     catch(e){
+//         console.log(e)
+//     }
     
-}
+// }
 
-main()
+// main()
 
 module.exports={
     users:userdata,
