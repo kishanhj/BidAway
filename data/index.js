75ccba5616de79dcc481347bec63ae666434c2d4
Abin007 <abinmartin.jones@gmail.com>
2019-12-13 07:11:42 -0500

    null commit

--- data/index.js
+++ data/index.js
@@ -12,9 +12,7 @@ const ratings=require("./ratings")
 //         startTime: new Date('2019-12-19 11:11')
 //     }
 //         // const comment= await items.addItem(item.name,item.category,item.description,item.startPrice,item.startTime,"5df2986fd8ea493e20b732ea")
-//        const rating= await ratings.createrating("5df297a72d325e3dc28d00b2","5df2ba8c2e51f3471a1b8aa4","3")
-//        console.log(rating)
-       
+//        const rating= await ratings.createrating("5df29845d8ea493e20b732e9","5df2ba8c2e51f3471a1b8aa4","2")
         
         
 
